92749725875fbc0a38a5fb29a2341bc254e33dc0
Djurre <djurre10@hotmail.com>
2019-01-29 15:59:03 +0100

    Mvc Maybe

--- src/Parkeersimulator/Car.java
+++ src/Parkeersimulator/Car.java
@@ -10,7 +10,7 @@ public abstract class Car {
     private boolean hasToPay;
 
     /**
-     * Constructor for objects of class Car
+     * Constructor for objects of class CarController
      */
     public Car() {
 
