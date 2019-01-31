862b699dabb9aa09982056f2c6a7e3a0faddd54d
Djurre <djurre10@hotmail.com>
2019-01-31 18:51:56 +0100

    hoihohikl

--- spark/src/parkeersimulator/model/Financing.java
+++ spark/src/parkeersimulator/model/Financing.java
@@ -1,18 +1,27 @@
+
 package parkeersimulator.model;
 
 
-import java.io.*;
-import sun.audio.*;
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
-public class Financing {
+
+public class Financing implements LineListener {
 
     private float currentWeekIncome;
     private float currentDayIncome;
     private float upcomingIncome;
+    boolean playCompleted;
 
     public float singleDayPrice = 5.50f;
 
@@ -25,28 +34,65 @@ public class Financing {
         this.upcomingIncome -= amountToAdd;
 
         addToWeekIncome(amountToAdd);
-
-        //playSound("C:/assets/kaching.wav");
+        Financing player = new Financing();
+        player.play("D:\\Programmeren\\Java\\VroemVroem\\spark\\assets\\kaching.wav");
 
 
     }
-    /*
-    private void playSound(final String url) {
-        try
-        {
-            // get the sound file as a resource out of my jar file;
-            // the sound file must be in the same directory as this class file.
-            // the input stream portion of this recipe comes from a javaworld.com article.
-            InputStream inputStream = getClass().getResourceAsStream(url);
-            AudioStream audioStream = new AudioStream(inputStream);
-            AudioPlayer.player.start(audioStream);
+    void play(String audioFilePath) {
+        File audioFile = new File(audioFilePath);
+
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+            AudioFormat format = audioStream.getFormat();
+
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+
+            Clip audioClip = (Clip) AudioSystem.getLine(info);
+
+            audioClip.addLineListener(this);
+
+            audioClip.open(audioStream);
+
+            audioClip.start();
+
+            while (!playCompleted) {
+                // wait for the playback completes
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            audioClip.close();
+
+        } catch (UnsupportedAudioFileException ex) {
+            System.out.println("The specified audio file is not supported.");
+            ex.printStackTrace();
+        } catch (LineUnavailableException ex) {
+            System.out.println("Audio line for playing back is unavailable.");
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            System.out.println("Error playing the audio file.");
+            ex.printStackTrace();
         }
-        catch (Exception e)
-        {
-            // a special way i'm handling logging in this application
-            System.out.println( e );
+
+    }
+
+    public void update(LineEvent event) {
+        LineEvent.Type type = event.getType();
+
+        if (type == LineEvent.Type.START) {
+            System.out.println("Playback started.");
+
+        } else if (type == LineEvent.Type.STOP) {
+            playCompleted = true;
+            System.out.println("Playback completed.");
         }
-    }*/
+
+    }
 
     public void addToUpcoming(float amountToAdd) {
         this.upcomingIncome += amountToAdd;
